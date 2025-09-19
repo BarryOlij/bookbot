fae9935c3d6c024131b02f976875ee25f1a73842
Barry Olij <barry@unloc.nl>
2025-09-19 08:52:35 +0200

    completed CH3 L2

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from stats import count_words
-from stats import count_chars
+from stats import sorted_chars
+import sys
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    if(len(sys.argv) < 2):
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     book_wordcount = count_words(book_text)
+    sorted_list = sorted_chars(book_text)
 
-    print(f"{book_wordcount} words found in the document")
-    print(count_chars(book_text))
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {book_path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {book_wordcount} total words')
+    print('--------- Character Count -------')    
+    for item in sorted_list:
+        ch = item["char"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
+    print('============= END ===============')
 
 def get_book_text(path):
     with open(path) as file:
--- stats.py
+++ stats.py
@@ -10,3 +10,18 @@ def count_chars(book):
         else:
             char_lib[char] = 1
     return char_lib
+    
+def key_by_count(item):
+    return item["num"]
+
+def sorted_chars(book):
+    sorted_chars = []
+    raw_chars = count_chars(book)
+
+    for ch, n in raw_chars.items():
+        sorted_chars.append({"char": ch, "num": n})
+    
+
+    sorted_chars.sort(reverse=True, key=key_by_count)
+
+    return sorted_chars
